9dbb274f95cd8b54a06a49a102991434666e9dfd
deathcap <deathcap@gmx.co.uk>
2014-04-15 19:44:08 -0700

    Move shader setup from game-shell-voxel/voxel-mipmap-demo and turn into a plugin

--- aoshader.js
+++ aoshader.js
@@ -1,8 +1,80 @@
 var fs = require("fs")
 var createShader = require("gl-shader")
 
-module.exports  = function(gl) {
-  return createShader(gl,
+var mat4 = require('gl-matrix').mat4
+
+module.exports = function(game, opts) {
+  return new ShaderPlugin(game, opts);
+};
+module.exports.pluginInfo = {
+  clientOnly: true,
+  loadAfter: ['voxel-stitch'],
+};
+
+function ShaderPlugin(game, opts) {
+  this.shell = game.shell;
+
+  this.stitcher = game.plugins.get('voxel-stitch');
+  if (!this.stitcher) throw new Error('voxel-shader requires voxel-stitch plugin'); // for tileCount uniform below
+
+  this.enable();
+}
+
+ShaderPlugin.prototype.enable = function() {
+  this.shell.on('gl-init', this.onInit = this.ginit.bind(this));
+  this.shell.on('gl-render', this.onRender = this.render.bind(this));
+};
+
+ShaderPlugin.prototype.disable = function() {
+  this.shell.removeListener('gl-init', this.onInit);
+  this.shell.removeListener('gl-render', this.onRender);
+};
+
+ShaderPlugin.prototype.ginit = function() {
+  this.shader = this.createAOShader();
+};
+
+ShaderPlugin.prototype.render = function() {
+  var gl = this.shell.gl
+
+  //Calculation projection matrix
+  var projection = mat4.perspective(new Float32Array(16), Math.PI/4.0, this.shell.width/this.shell.height, 1.0, 1000.0) // TODO: only update on resize event
+  var model = mat4.identity(new Float32Array(16)) // TODO: move out of loop. or merge to modelView?
+  var view = this.shell.camera.view() // TODO: expose camera through a plugin instead?
+
+  // for voxel-wireframe rendering TODO: refactor
+  this.projection = projection
+  this.model = model
+  this.view = view
+
+  gl.enable(gl.CULL_FACE)
+  gl.enable(gl.DEPTH_TEST)
+
+  //Bind the shader
+  var shader = this.shader
+  shader.bind()
+  shader.attributes.attrib0.location = 0
+  shader.attributes.attrib1.location = 1
+  shader.uniforms.projection = projection
+  shader.uniforms.view = view
+  shader.uniforms.model = model
+  shader.uniforms.tileCount = this.stitcher.tileCount
+
+  // TODO: relocate variables off of game.shell (texture, meshes)
+
+  var texture = this.stitcher.texture
+  if (texture) shader.uniforms.tileMap = texture.bind() // texture might not have loaded yet
+
+  for (var i = 0; i < this.shell.meshes.length; ++i) {
+    var mesh = this.shell.meshes[i];
+    mesh.triangleVAO.bind()
+    gl.drawArrays(gl.TRIANGLES, 0, mesh.triangleVertexCount)
+    mesh.triangleVAO.unbind()
+  }
+};
+
+ShaderPlugin.prototype.createAOShader = function() {
+  return createShader(this.shell.gl,
     fs.readFileSync(__dirname+"/lib/ao.vsh"),
     fs.readFileSync(__dirname+"/lib/ao.fsh"))
-}
+};
